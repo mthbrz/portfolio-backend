79853a2bf7d3434dd65095346f965a59ffd94505
Mathilde <mathilde.bruzat@gmail.com>
2025-09-12 16:08:34 +0200

    ajout cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("dkhitxegv"),
+        api_key: env("967123589829898"),
+        api_secret: env("5lC7EjdEqN4ZVQsYw_P4rAHkDyg"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
