69529c58858d70413939f98b54e003c92d1e10ce
Mathilde <mathilde.bruzat@gmail.com>
2025-10-14 16:40:53 +0200

    modifications sqlite

--- config/database.js
+++ config/database.js
@@ -1,34 +1,11 @@
 const path = require('path');
 
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres'); 
-
-  const connections = {
-    postgres: {
-      connection: {
-        host: env('DATABASE_HOST'),           
-        port: env.int('DATABASE_PORT', 5432),  
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false),  
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
