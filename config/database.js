8a56954920de68e28fc06fff3485e7f616b32372
Mathilde <mathilde.bruzat@gmail.com>
2025-10-16 10:57:01 +0200

    ajout strapi local

--- config/database.js
+++ config/database.js
@@ -1,15 +1,29 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'ep-xxxx.eu-central-1.aws.neon.tech'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'neondb'),
-      user: env('DATABASE_USERNAME', 'mathi'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: {
-        rejectUnauthorized: false, 
-      },
-    },
-  },
-});
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const isProduction = env('NODE_ENV') === 'production';
+
+  return {
+    connection: isProduction
+      ? {
+          client: 'postgres',
+          connection: {
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            ssl: {
+              rejectUnauthorized: false, // ✅ pour Render / Neon
+            },
+          },
+        }
+      : {
+          client: 'sqlite',
+          connection: {
+            filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+          },
+          useNullAsDefault: true,
+        },
+  };
+};
