28337e4ed5a1d14924a83ea557e31215b3644296
Mathilde <mathilde.bruzat@gmail.com>
2025-10-16 10:22:28 +0200

    changement pour PostgreSQL

--- config/database.js
+++ config/database.js
@@ -2,10 +2,20 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
+    client: env('DATABASE_CLIENT', 'sqlite'),
+    connection:
+      env('DATABASE_CLIENT') === 'sqlite'
+        ? {
+            filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+          }
+        : {
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            ssl: env.bool('DATABASE_SSL', true),
+          },
+    useNullAsDefault: env('DATABASE_CLIENT') === 'sqlite',
   },
 });
--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,13 @@
+console.log("CLOUDINARY CONFIG:", {
+  name: process.env.CLOUDINARY_NAME,
+  key: process.env.CLOUDINARY_KEY,
+  secret: process.env.CLOUDINARY_SECRET ? 'OK' : 'Missing',
+});
+
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: '@strapi/provider-upload-cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
