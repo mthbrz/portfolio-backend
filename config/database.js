3e32c2f80c2fe674a7a641b1cd29f79c19944db3
Mathilde <mathilde.bruzat@gmail.com>
2025-10-16 10:40:23 +0200

    modification database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,15 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'sqlite'),
-    connection:
-      env('DATABASE_CLIENT') === 'sqlite'
-        ? {
-            filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-          }
-        : {
-            host: env('DATABASE_HOST'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME'),
-            user: env('DATABASE_USERNAME'),
-            password: env('DATABASE_PASSWORD'),
-            ssl: env.bool('DATABASE_SSL', true),
-          },
-    useNullAsDefault: env('DATABASE_CLIENT') === 'sqlite',
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'ep-xxxx.eu-central-1.aws.neon.tech'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'neondb'),
+      user: env('DATABASE_USERNAME', 'mathi'),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: {
+        rejectUnauthorized: false, 
+      },
+    },
   },
 });
